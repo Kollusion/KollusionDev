5052efd45a1cbbdc5af3c8ce520b1d408284477a
Kollusion <kollusion.dark@gmail.com>
2025-10-25 16:05:11 +0700

    fix locale

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing';
- 
+
 export const routing = defineRouting({
-  locales: ['en', 'ru', 'uk'],
- 
-  defaultLocale: 'en'
+    locales: ['en', 'ru'],
+
+    defaultLocale: 'ru'
 });
